3f20e7fefe88ec84e9488cb32477e93e7b380176
DiegoAdolfo <diego2003coila@gmail.com>
2023-06-20 19:17:43 -0500

    soyDiego

---
+++ unid1/s04_val_js_css/js/form04valida.js
@@ -0,0 +1,9 @@
+const fnValidar04 = () => {
+
+    var elementoPref = document.getElementsByName("chkPrefer1");
+	var contador=0;
+	for(var i=0; 1 < elementoPref.length; i++)
+		
+
+
+}
---
+++ unid1/s05_aplic_val/js/form02funciones.js
@@ -0,0 +1,16 @@
+function fnValidar02()
+{
+    let xNombre = document.getElementById("txtNombres").value;
+    if (xNombre == "" || /^\s+$/.test(xNombre)) {
+        alert("Escriba nombre");
+        return false;
+    }
+ 
+    let xApellido = document.getElementById("txtApellidos").value;
+    if (xApellido== "" || /^\s+$/.test(xApellido)) {
+        alert("Escriba apellido");
+        return false;
+    }
+
+    return true;
+}
